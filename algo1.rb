a4f74690ef8c578a1e5f4fd9234542fc518473b7
Glenn Y. Rolland <glenux@gmail.com>
2011-08-28 16:24:49 +0200

    application & algo1: renaming.

--- algo1.rb
+++ algo1.rb
@@ -4,16 +4,48 @@ $DEBUG = true
 $VERBOSE= true
 
 require 'pp'
+
 require 'rubygems'
+#require 'sinatra'
 require 'json'
-require 'mendeley'
+
+require 'vocabulari-se/config'
 require 'vocabulari-se/cache'
+require 'vocabulari-se/utils'
+require 'mendeley'
+require 'wikipedia'
 
-config = JSON.load File.open 'config.json'
-cache = VocabulariSe::DirectoryCache.new config["cache_dir"], (60 * 60 * 24)
-client = Mendeley::Client.new( config["consumer_key"], cache )
+json = JSON.load File.open 'config.json'
+config = VocabulariSe::Config.new json
 
-STDERR.print "input tag ? "
-t0 = STDIN.gets
+print "tag ? "
+intag = STDIN.gets.strip
 
-related_tags = Mendeley::Tag.related_tags client, t0
+
+# Association audacieuse
+workspace = {}
+documents = Set.new
+related_tags = VocabulariSe::Utils.related_tags config, intag
+related_tags.each do |reltag|
+	# sum of views for all documents
+	views = 1
+	apparitions = 0
+	VocabulariSe::Utils.related_documents_multiple config, [intag, reltag] do |doc|
+		views += 1
+		apparitions += 1
+	end
+	slope =  apparitions / views
+	workspace[reltag] = {
+		:views => views,
+		:apparitions => apparitions,
+		:slope => slope
+	}
+end
+pp workspace
+
+# sort workspace keys (tags) by slope
+result = workspace.sort{ |a,b| a[1][:slope] <=> b[1][:slope] }
+
+# FIXME : limit to 3 or 5 results only
+pp result
+
