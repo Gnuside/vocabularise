34c60ef775f07d6423a9dbb6c7c621f4e44e9ef8
Glenn Y. Rolland <glenux@gmail.com>
2011-08-28 16:25:34 +0200

    run.sh: run algo1.

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 export RUBYLIB=`pwd`/../Ruby-Spore/lib:lib
 #bundle exec rackup config.ru -o 0.0.0.0 -p 9393
-ruby application.rb
+ruby algo1.rb
+#ruby algo2.rb
+#ruby algo3.rb
 #bundle exec rackup config.ru -o 127.0.0.1 -p 9001
 #bundle exec rackup config.ru -p 9393
 
