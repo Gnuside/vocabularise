3afa00909eca55866f5fe42749dc341570f56af2
Glenn Y. Rolland <glenux@gmail.com>
2011-09-04 16:13:59 +0200

    Rewrite algo3 based on new algo1 .

--- algo3.rb
+++ algo3.rb
@@ -20,52 +20,84 @@ require 'mendeley'
 require 'wikipedia'
 
 
-# limit the number of considered articles for computation
-ARTICLE_LIMIT = 3
-def tag_hotness config, tags_arr
-	search_expr = tags_arr.sort.join(' AND ')
-	puts search_expr
-
-	resp_json =  config.wikipedia_client.search( search_expr )
-	resp = JSON.parse resp_json
-
-	limit = ARTICLE_LIMIT
-	resp["query"]["search"].each do |article_desc|
-		talk_title = "Talk:%s" % article_desc["title"]
-		#pp article_desc
-		puts "  - " + talk_title
-		limit -= 1
-		break if limit <= 0
-	end
-end
 
 json = JSON.load File.open 'config.json'
 config = VocabulariSe::Config.new json
 
-puts "Algo II"
+puts "Algo III"
 print "tag ? "
 intag = STDIN.gets.strip
 
+Indent.more
+
 
 # Association audacieuse
-workspace = {}
+tag_workspace = {}
+discipline_workspace = Hash.new({
+	:ref => nil,
+	:value => 0,
+	:count => 0
+})
+
 documents = Set.new
 related_tags = VocabulariSe::Utils.related_tags config, intag
-related_tags.each do |reltag,reltag_count|
-	hotness = tag_hotness( config, [reltag, intag] )
+puts "Algo III - related tags"
+pp related_tags
 
-	#workspace[reltag] = {
-	#	:hotness => hotness
-	#}
+related_tags.each do |reltag,reltag_count|
+	# sum of views for all documents
+	views = 1
+	apparitions = reltag_count
+
+	hit_count = 0
+	limit = 2
+
+	# workspace de disciplines
+	VocabulariSe::Utils.related_documents_multiple config, [intag, reltag] do |doc|
+		# FIXME: lister les disciplines du document
+		# pour chaque discipline :
+		#  * ajouter les % de readers
+		#  * incrément de la discipline
+		disciplines = doc.disciplines(config.mendeley_client)
+		pp disciplines
+=begin
+		disciplines.each do |discipline|
+			disc_name = discipline["name"]
+			disc_id = discipline["id"]
+			disc_value = discipline["value"]
+
+			discipline_workspace[disc_name][:ref] ||= discipline
+			discipline_workspace[disc_name][:value] += disc_value
+			discipline_workspace[disc_name][:count] += 1
+		end
+		# FIXME : associer à chaque tag les disciplines trouvées
+=end
+		# limit to X real hits
+		hit_count += 1 #unless doc.cached?
+		puts "Algo - hit_count = %s" % hit_count
+		break if hit_count > limit
+	end
+=begin
+	slope =  apparitions.to_f / views.to_f
+	tag_workspace[reltag] = {
+		:views => views,
+		:apparitions => apparitions,
+		:slope => slope
+	}
+	exit 1
+=end
 end
 exit 1
 
-puts "AlgoII - all tags :"
-pp workspace.keys
+puts "AlgoIII - all disciplines :"
+pp discipline_workspace
+#.keys
 
-# sort workspace keys (tags) by slope
-result = workspace.sort{ |a,b| a[1][:slope] <=> b[1][:slope] }
+exit 1
+# sort workspace keys (tags) by increasing slope 
+result = workspace.sort{ |a,b| a[1][:slope] <=> b[1][:slope] }.reverse
 
 # FIXME : limit to 3 or 5 results only
 pp result[0..4]
+
 
