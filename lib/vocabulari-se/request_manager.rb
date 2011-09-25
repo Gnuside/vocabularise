915ae1218abe55ba65a2b86e2cb5d0b142079643
Glenn Y. Rolland <glenux@gmail.com>
2011-09-25 21:14:18 +0200

    vocabulari-se: Added files for request handling.

---
+++ lib/vocabulari-se/request_manager.rb
@@ -0,0 +1,17 @@
+
+module VocabulariSe
+
+	class RequestManager
+
+
+	def step
+
+		# FIXME: get next request 
+		# - sorted by priority 
+		# - sorted by creation time
+		# make the request
+		#
+	end
+
+end
+
