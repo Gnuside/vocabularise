1c84b2e26b9ee37e17aa2f84f6ec4681eef6f4b5
Glenn Y. Rolland <glenux@gmail.com>
2011-09-25 21:19:06 +0200

    base: Added status pages for request queue & cache.

--- lib/base.rb
+++ lib/base.rb
@@ -1,42 +1,54 @@
 
-class API < Sinatra::Base
+module VocabulariSe
+	class API < Sinatra::Base
 
-	# Index page
-	get "/" do
-		raise NotImplementedError
-	end
-
-
-	# Return the result page for given search expression
-	get "/search/:search" do
-		# FIXME: use cache for search
-		raise NotImplementedError
-	end
+		# Index page
+		get "/" do
+			raise NotImplementedError
+		end
 
 
-	# Return information about mendeley documents for tag tag :tag 
-	# restricted to search :search
-	get "/tag/:tag/mendeley_doc/:search" do
-		# FIXME: use cache for search/tag
-		raise NotImplementedError
-	end
+		# Return the result page for given search expression
+		get "/search/:search" do
+			# FIXME: use cache for search
+			raise NotImplementedError
+		end
 
 
-	# Return information about mendeley disciplines for tag tag :tag 
-	# restructed to search :search
-	get "/tag/:tag/mendeley_disc/:search" do
-		# FIXME: use cache for search/tag
-		raise NotImplementedError
-	end
+		# Show request queue
+		get "/status/request_queue" do
+		end
 
 
-	# Return information about wikipedia pages for tag tag :tag 
-	# restricted to search :search
-	get "/tag/:tag/wikipedia_page/:search" do
-		# FIXME: use cache for search/tag
-		raise NotImplementedError
+		# Show current cache
+		get "/status/cache" do 
+		end
+
+
+		# Return information about mendeley documents for tag tag :tag 
+		# restricted to search :search
+		get "/tag/:tag/mendeley_doc/:search" do
+			# FIXME: use cache for search/tag
+			raise NotImplementedError
+		end
+
+
+		# Return information about mendeley disciplines for tag tag :tag 
+		# restructed to search :search
+		get "/tag/:tag/mendeley_disc/:search" do
+			# FIXME: use cache for search/tag
+			raise NotImplementedError
+		end
+
+
+		# Return information about wikipedia pages for tag tag :tag 
+		# restricted to search :search
+		get "/tag/:tag/wikipedia_page/:search" do
+			# FIXME: use cache for search/tag
+			raise NotImplementedError
+		end
+
+
 	end
 
 end
-
-
