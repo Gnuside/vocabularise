7755a96fb9e2d42e0f045e7cc1ca3390fcec6d8c
Glenn Y. Rolland <glenux@gmail.com>
2011-09-27 22:09:32 +0200

    vocabularise/config: Fixed parsing of JSON.

--- lib/vocabularise/config.rb
+++ lib/vocabularise/config.rb
@@ -1,8 +1,10 @@
 
-require 'vocabulari-se/wikipedia'
+require 'vocabularise/wikipedia'
 
 module VocabulariSe
 	class Config
+
+		class ConfigurationError < RuntimeError ; end
 
 		# api clients
 		attr_reader :mendeley_client
@@ -29,39 +31,40 @@ module VocabulariSe
 		end
 
 		def load_json json
-			raise ConfigurationError, "no cache_dir specified" unless json.include "cache_dir"
+			raise ConfigurationError, "no cache_dir specified" unless json.include? "cache_dir"
 			@cache = VocabulariSe::DirectoryCache.new json["cache_dir"], (60 * 60 * 24)
 
-			raise ConfigurationError, "no consumer_key specified" unless json.include "consumer_key"
+			raise ConfigurationError, "no consumer_key specified" unless json.include? "consumer_key"
 			@mendeley_client = Mendeley::Client.new( json["consumer_key"], cache )
 			@wikipedia_client = Wikipedia::Client.new
 			@wikipedia_client.extend(WikipediaFix)
 
-			raise ConfigurationError, "no db_adapter specified" unless json.include "db_adapter"
-			raise ConfigurationError, "no db_database specified" unless json.include "db_database"
+			raise ConfigurationError, "no db_adapter specified" unless json.include? "db_adapter"
+			raise ConfigurationError, "no db_database specified" unless json.include? "db_database"
+
 			case json["db_adapter"]
 			when 'sqlite' then                                                                                          
 				@database = {                                                                                                
 					"adapter"   => 'sqlite3',                                                                           
-					"database"  => json["db_database"]
+					"database"  => json["db_database"],
 					"username"  => "",                                                                                  
-						"password"  => "",                                                                                  
-						"host"      => "",                                                                                  
-						"timeout"   => 15000                                                                                
+					"password"  => "",                                                                                  
+					"host"      => "",                                                                                  
+					"timeout"   => 15000                                                                                
 				}                                                                                                       
 			when 'mysql' then                                                                                           
 				raise ConfigurationError, "no db_password specified" unless json.include "db_password"
 				raise ConfigurationError, "no db_username specified" unless json.include "db_username"
 				raise ConfigurationError, "no db_host specified" unless json.include "db_host"
-				@database = {                                                                                                
+				@database = {	
 					"adapter"   => 'mysql',                                                                             
 					"database"  => json["db_database"],
 					"username"  => json["db_username"],                                                                           
 					"password"  => json["db_password"],
 					"host"      => json["db_host"]
-				}                                                                                                       
+				}	
 			else
-				raise UnknownAdapter, @adapter
+				#raise UnknownAdapter, @adapter
 			end
 
 		end
