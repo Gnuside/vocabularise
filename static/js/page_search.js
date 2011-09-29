6adb1355a5f34a2bdd84d35cc5c60b79abde0a14
Glenn Y. Rolland <glenux@gmail.com>
2011-09-29 13:50:05 +0200

    ui: Display results from RESTful API request.

--- static/js/page_search.js
+++ static/js/page_search.js
@@ -1,5 +1,81 @@
 
+var COLOR_EXPECTED = [ 60, 165, 43 ]; // #3ca52b
+var COLOR_CONTROVERSIAL = [ 231, 80, 24 ]; // #e75018
+var COLOR_AGGREGATING= [ 0, 139, 208 ]; // #008bd0
+var COLOR_BLACK = [0,0,0];
+
+var TEXTSIZE_MIN = 0.6;
+var TEXTSIZE_MAX = 2;
+
+function show_resultlist( elem, resp, color ) {
+	//alert("got "+result);
+
+	elem.fadeOut(function() {
+		elem.empty();
+		for(var idx=0;idx<resp.result.length;idx++){
+			var tag = resp.result[idx][0];
+			var factor = ((idx + 0) / resp.result.length);
+			var res_size = TEXTSIZE_MAX - ( factor * (TEXTSIZE_MAX - TEXTSIZE_MIN));
+			var res_rgb = [ 
+				Math.floor(color[0] - ( factor * (color[0] - COLOR_BLACK[0]) )),
+				Math.floor(color[1] - ( factor * (color[1] - COLOR_BLACK[1]) )),
+				Math.floor(color[2] - ( factor * (color[2] - COLOR_BLACK[2]) ))
+			];
+			elem.append('<li style="'+
+				'font-size: '+res_size+'em; '+
+				'color: rgb(' + res_rgb[0] + ',' + res_rgb[1] + ',' + res_rgb[2] + ');'+ 
+				'">' + tag + '</li>');
+		}
+		// make change happen
+		elem.fadeIn();
+	});
+}
+
+function load_expected( query ) {
+	$.ajax({
+		url: "/search/expected?query=" + query,
+		context: $('#list_expected'),
+		dataType: 'json',
+		success: function( resp ){ show_resultlist( $(this), resp, COLOR_EXPECTED ); },
+		error: function( req, error ) {
+			// setTimeout(2000, load_expected(query));
+		}
+	});
+}
+
+function load_controversial( query ) {
+	$.ajax({
+		url: "/search/controversial?query=" + query,
+		context: $('#list_controversial'),
+		dataType: 'json',
+		success: function( resp ){ show_resultlist( $(this), resp, COLOR_CONTROVERSIAL ); },
+		error: function( req, error ) {
+			// setTimeout(2000, load_expected(query));
+		}
+	});
+}
+
+function load_aggregating( query ) {
+	$.ajax({
+		url: "/search/aggregating?query=" + query,
+		context: $('#list_aggregating'),
+		dataType: 'json',
+		success: function( resp ){ show_resultlist( $(this), resp, COLOR_AGGREGATING ); },
+		error: function( req, error ) {
+			// setTimeout(2000, load_expected(query));
+		}
+	});
+}
+
 $(document).ready(function() {
+	/* set focus */
 	$('#searchwidget_query').focus();
+
+	/* run queries */
+	query = $('#searchwidget_oldquery').val();
+
+	load_expected( query );
+	load_controversial( query );
+	load_aggregating( query );
 });
 
