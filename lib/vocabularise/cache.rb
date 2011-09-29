d539dc2af682d0c34a33713f4433fe35e9eb4c2b
Glenn Y. Rolland <glenux@gmail.com>
2011-09-29 23:37:51 +0200

    vocabularise/cache: make cache class thread-safe.

--- lib/vocabularise/cache.rb
+++ lib/vocabularise/cache.rb
@@ -1,6 +1,7 @@
 
-require "common/indent"
-require "base64"
+require 'monitor'
+require 'common/indent'
+require 'base64'
 require 'fileutils'
 
 # FIXME: lock file
@@ -16,58 +17,67 @@ module VocabulariSe
 				FileUtils.mkdir_p @root
 			end
 			@debug = false
+			@monitor = Monitor.new
 		end
 
 
 		def include? key
-			path = "%s.info" % (_key_to_path key)
-			if File.exist? path then
-				created_at = nil
-				File.open path, "r" do |fh|
-					created_at = fh.gets.strip
+			@monitor.synchronize do 
+				path = "%s.info" % (_key_to_path key)
+				if File.exist? path then
+					created_at = nil
+					File.open path, "r" do |fh|
+						created_at = fh.gets.strip
+					end
+					diff = Time.now.to_i - created_at.to_i
+					return (diff < @timeout)
+				else
+					return false
 				end
-				diff = Time.now.to_i - created_at.to_i
-				return (diff < @timeout)
-			else
-				return false
 			end
 		end
 
 		# takes a HTTP::Message (response)
 		def []= key, resp
-			#puts "resp.body class = %s" % resp.body.class
-			#pp resp.body.inspect
+			@monitor.synchronize do 
+				#puts "resp.body class = %s" % resp.body.class
+				#pp resp.body.inspect
 
-			path = "%s.info" % (_key_to_path key)
-			File.open path, "w" do |fh|
-				fh.puts Time.now.to_i
-			end
+				path = "%s.info" % (_key_to_path key)
+				File.open path, "w" do |fh|
+					fh.puts Time.now.to_i
+				end
 
-			path = "%s.data" % (_key_to_path key)
-			rdebug "CACHE = %s" % path
-			File.open path, "w" do |fh|
-				fh.write Marshal.dump( resp )
+				path = "%s.data" % (_key_to_path key)
+				rdebug "CACHE = %s" % path
+				File.open path, "w" do |fh|
+					fh.write Marshal.dump( resp )
+				end
 			end
 		end
 
 		# return a HTTP::Message::Body
 		def [] key
-			path = "%s.data" % (_key_to_path key)
-			rdebug "CACHE = %s" % path
-			value = nil
-			File.open path, "r" do |fh|
-				value = Marshal.load fh.read
-			end 
-			resp = value
-			#HTTP::Message.new_response value
-			return resp
+			@monitor.synchronize do 
+				path = "%s.data" % (_key_to_path key)
+				rdebug "CACHE = %s" % path
+				value = nil
+				File.open path, "r" do |fh|
+					value = Marshal.load fh.read
+				end 
+				resp = value
+				#HTTP::Message.new_response value
+				return resp
+			end
 		end
 
 		def each &blk
-			d = Dir.new @root
-			d.each do |x|
-				next if x == '.' or x == '..'
-				yield x
+			@monitor.synchronize do 
+				d = Dir.new @root
+				d.each do |x|
+					next if x == '.' or x == '..'
+					yield x
+				end
 			end
 		end
 
