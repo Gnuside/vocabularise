e7033ace9c190e1fc1d8f1b850d4e39abc367a97
Glenn Y. Rolland <glenux@gmail.com>
2011-09-30 11:57:31 +0200

    vocabularise: Added a queue class.

---
+++ lib/vocabularise/queue.rb
@@ -0,0 +1,16 @@
+
+module VocabulariSe
+	class MemoryQueue
+		def enqueue key, value
+		end
+
+		def dequeue key
+		end
+
+		def include? key
+		end
+
+		# FIXME: do something for priority
+	end
+end
+
