e6f6e4aee7bda11301df640fe442542cdb1d6b28
Glenn Y. Rolland <glenux@gmail.com>
2011-10-02 19:10:04 +0200

    vocabularise/generic_cache: Throw NotImplementedErrors eveywhere ;)

--- lib/vocabularise/generic_cache.rb
+++ lib/vocabularise/generic_cache.rb
@@ -2,5 +2,26 @@
 module VocabulariSe
 	class GenericCache
 		include Enumerable
+
+		def initialize directory, timeout
+			raise NotImplementedError
+		end
+
+		def include? key
+			raise NotImplementedError
+		end
+
+		def []= key, resp
+			raise NotImplementedError
+		end
+
+		def [] key
+			raise NotImplementedError
+		end
+
+		def each &blk
+			raise NotImplementedError
+		end
+
 	end
 end
