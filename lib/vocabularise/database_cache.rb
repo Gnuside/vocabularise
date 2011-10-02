1d91dd879d38b57840fb57a958dd33b186a11889
Glenn Y. Rolland <glenux@gmail.com>
2011-10-02 19:10:38 +0200

    vocabularise/database_cache: Implement database cache entry.

--- lib/vocabularise/database_cache.rb
+++ lib/vocabularise/database_cache.rb
@@ -1,22 +1,39 @@
 
+require 'dm-core'
 require 'vocabularise/generic_cache'
 
 module VocabulariSe
+
+	class DatabaseCacheEntry
+		include DataMapper::Resource
+
+		property :id,   String, :key => true
+		property :data, String, :required => true 
+		property :created_at, Integer, :required => true                        
+		property :expires_at, Integer, :required => true                        
+
+	end
+
 	class DatabaseCache < GenericCache
 
 		def initialize directory, timeout
+			super
 		end
 
 		def include? key
+			raise NotImplementedError
 		end
 
 		def []= key, resp
+			raise NotImplementedError
 		end
 
 		def [] key
+			raise NotImplementedError
 		end
 
 		def each &blk
+			raise NotImplementedError
 		end
 	end
 end
