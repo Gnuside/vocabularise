6a71cb0ccebaba3fe093efbce323e362b00a681b
Glenn Y. Rolland <glenux@gmail.com>
2011-10-02 20:33:49 +0200

    vocabularise/database_cache: implemented database_cache.

--- lib/vocabularise/database_cache.rb
+++ lib/vocabularise/database_cache.rb
@@ -2,6 +2,8 @@
 require 'dm-core'
 require 'vocabularise/generic_cache'
 
+require 'monitor'
+
 module VocabulariSe
 
 	class DatabaseCacheEntry
@@ -16,24 +18,56 @@ module VocabulariSe
 
 	class DatabaseCache < GenericCache
 
-		def initialize directory, timeout
-			super
+		def initialize timeout
+			@timeout = timeout
+			@monitor = Monitor.new
 		end
 
 		def include? key
-			raise NotImplementedError
+			now = Time.now
+			req = { :id => key,
+		   			:expires_at.lt => now.to_i}
+			resp = DatabaseCacheEntry.first req
+			return (not resp.nil?)
 		end
 
-		def []= key, resp
-			raise NotImplementedError
+		def []= key, data
+			DatabaseCacheEntry.transaction do
+				now = Time.now
+
+				resp = DatabaseCacheEntry.get key
+				resp.destroy if resp
+
+				create = { 
+					:id => key,
+					:data => data,
+					:created_at => now.to_i,
+					:expires_at => now.to_i + @timeout,
+				}
+				resp = DatabaseCacheEntry.create create
+			end
+		rescue DataMapper::SaveFailureError => e
+			STDERR.puts e.message
+			raise RuntimeError, "unable to set data"
 		end
 
 		def [] key
-			raise NotImplementedError
+			now = Time.now
+			req = { 
+				:id => key,
+				:expires_at.lt => now.to_i
+			}
+			resp = DatabaseCacheEntry.first req
+			return resp
 		end
 
 		def each &blk
-			raise NotImplementedError
+			now = Time.now
+			req = { 
+				:expires_at.lt => now.to_i
+			}
+			resp = DatabaseCacheEntry.all req
+			resp.each do |x| yield x ; end
 		end
 	end
 end
