0bdca7e966ab424bb5a0507eb9b3f1462039ccfb
Glenn Y. Rolland <glenux@gmail.com>
2011-10-02 21:17:29 +0200

    vocabularise/config: Initialize mendeley/wikipedia client after cache.

--- lib/vocabularise/config.rb
+++ lib/vocabularise/config.rb
@@ -5,6 +5,10 @@ require 'vocabularise/wikipedia'
 require 'mendeley'
 require 'wikipedia'
 
+require 'datamapper'
+require 'dm-core'                                                               
+
+
 module VocabulariSe
 	class Config
 
@@ -38,12 +42,7 @@ module VocabulariSe
 		end
 
 		def load_json json
-			raise ConfigurationError, "no cache_dir specified" unless json.include? "cache_dir"
-
-			raise ConfigurationError, "no consumer_key specified" unless json.include? "consumer_key"
-			@mendeley_client = Mendeley::Client.new( json["consumer_key"], cache )
-			@wikipedia_client = Wikipedia::Client.new
-			@wikipedia_client.extend(WikipediaFix)
+			#raise ConfigurationError, "no cache_dir specified" unless json.include? "cache_dir"
 
 			raise ConfigurationError, "no db_adapter specified" unless json.include? "db_adapter"
 			raise ConfigurationError, "no db_database specified" unless json.include? "db_database"
@@ -76,16 +75,25 @@ module VocabulariSe
 
 			DataMapper::Logger.new(STDERR, :info)
 			pp @database
+			DataMapper.finalize
 			DataMapper.setup(:default, @database)
+			DataMapper::Model.raise_on_save_failure = true                      
+			DataMapper.auto_upgrade!                                            
+
 
 			# setup cache & queue
 			# 2 hours
-			@cache = VocabulariSe::DatabaseCache.new json["cache_dir"], (60 * 60 * 2)
+			@cache = VocabulariSe::DatabaseCache.new(60 * 60 * 2)
 			# 1 day
 			#@cache = VocabulariSe::DirectoryCache.new json["cache_dir"], (60 * 60 * 24)
 			
 			# simple queue model
 			@queue = VocabulariSe::MemoryQueue.new
+
+			raise ConfigurationError, "no consumer_key specified" unless json.include? "consumer_key"
+			@mendeley_client = Mendeley::Client.new( json["consumer_key"], cache )
+			@wikipedia_client = Wikipedia::Client.new
+			@wikipedia_client.extend(WikipediaFix)
 		end
 	end
 end
