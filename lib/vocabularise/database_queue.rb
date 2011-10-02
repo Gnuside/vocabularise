1cdb46cff57bcf87adf73a2a0a3fc95e6dafe7fe
Glenn Y. Rolland <glenux@gmail.com>
2011-10-02 23:58:20 +0200

    vocabularise/database_queue: first implementation of database_queue.

--- lib/vocabularise/database_queue.rb
+++ lib/vocabularise/database_queue.rb
@@ -1,9 +1,79 @@
 
+
+require 'dm-core'
 require 'vocabularise/generic_queue'
 
 module VocabulariSe
-	class MemoryQueue < GenericQueue
+
+	class DatabaseQueueEntry
+		include DataMapper::Resource
+
+		property :id,   String, :key => true
+		property :handler, String, :key => true
+		property :data, String, :required => true 
+		property :priority, Integer, :required => true                        
+
+	end
+
+	class DatabaseQueue < GenericQueue
+		#
 		# FIXME: do something for priority
+
+		def initialize
+		end
+
+		def include? key, handler
+			now = Time.now
+			req = { :id => key }
+			resp = DatabaseQueueEntry.first req
+			return (not resp.nil?)
+		end
+
+=begin
+		def []= key, resp
+			DatabaseQueueEntry.transaction do
+				now = Time.now
+
+				resp = DatabaseQueueEntry.get key
+				resp.destroy if resp
+
+				req_create = { 
+					:id => key,
+					:data => Marshal.dump( data ),
+					:created_at => now.to_i,
+					:expires_at => now.to_i + @timeout,
+				}
+				resp = DatabaseQueueEntry.create req_create
+				resp.save
+
+				self.include? key
+			end
+		rescue DataMapper::SaveFailureError => e
+			STDERR.puts e.message
+			raise RuntimeError, "unable to set data"
+		end
+
+		def [] key
+			now = Time.now
+			req = { 
+				:id => key,
+				:expires_at.gt => now.to_i
+			}
+			resp = DatabaseQueueEntry.first req
+			if resp then return Marshal.load( resp.data )
+			else return nil
+			end
+		end
+=end
+
+		def each &blk
+			now = Time.now
+			resp = DatabaseQueueEntry.all
+			resp.each do |x| 
+				yield x 
+			end
+			raise RuntimeError
+		end
 	end
 end
 
