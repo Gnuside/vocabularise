e13375a7a8223d909665c98df0fd34b3e3a7536c
Glenn Y. Rolland <glenux@gmail.com>
2011-10-02 21:52:13 +0200

    vocabularise/memory_queue: Added basic implementation.

--- lib/vocabularise/memory_queue.rb
+++ lib/vocabularise/memory_queue.rb
@@ -3,7 +3,35 @@ require 'vocabularise/generic_queue'
 
 module VocabulariSe
 	class MemoryQueue < GenericQueue
-		# FIXME: do something for priority
+
+		def initialize
+			@queue = []
+			@content = {}
+		end
+
+		def include? key
+			@queue.include? key
+		end
+
+		def []= key, data
+			@queue << key
+			@content[key] = data
+		end
+
+		def [] key
+			@content[key]	
+		end
+
+		def each &blk
+			@queue.each do |x|
+				yield x
+			end
+		end
+
+		def << key
+			@queue << key
+			@content[key] = nil
+		end
 	end
 end
 
