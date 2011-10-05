9385b673fa9ba0f232e8d1d0b65627d593e1b9d3
Glenn Y. Rolland <glenux@glenux.net>
2011-10-05 17:32:19 +0200

    vocabularise/utils: cleanup related tags search (split in 3 submethods).

--- lib/vocabularise/utils.rb
+++ lib/vocabularise/utils.rb
@@ -24,18 +24,22 @@ module VocabulariSe
 			rdebug "VocabulariSe::Utils.related_documents config, %s" % intag
 			documents = Set.new
 			hit_count = 0
-			Mendeley::Document.search_tagged config.mendeley_client, intag do |doc|
-				raise RuntimeError, "nil document" if doc.nil?
-				raise RuntimeError, "nil document" if doc.kind_of? Array
-				if block_given? then
-					pp doc if @debug or @@debug ;
-					yield doc 
-				end
-				documents.add doc
+			begin
+				Mendeley::Document.search_tagged config.mendeley_client, intag do |doc|
+					raise RuntimeError, "nil document" if doc.nil?
+					raise RuntimeError, "nil document" if doc.kind_of? Array
+					if block_given? then
+						pp doc if @debug or @@debug ;
+						yield doc 
+					end
+					documents.add doc
 
-				hit_count += 1 unless doc.cached?
-				rdebug "hit_count = %s" % hit_count
-				break if hit_count > limit
+					hit_count += 1 unless doc.cached?
+					rdebug "hit_count = %s" % hit_count
+					break if hit_count > limit
+				end
+			rescue Mendeley::Client::DeferredRequest => e
+				# stop now :(
 			end
 			return documents
 		end
@@ -75,78 +79,88 @@ module VocabulariSe
 		end
 		
 
+		#
+		# Search related tags on mendeley
+		#
+		def self.related_tags_mendeley config, intag, limit=RELATED_TAGS_DEFAULT_HITLIMIT
+			tags = Hash.new 0
+
+			begin
+				hit_count = 0
+				# using the API
+				Mendeley::Document.search_tagged config.mendeley_client, intag do |doc|
+					document_tags = doc.tags config.mendeley_client
+					rdebug "Merge document tags"
+					rdebug "common tags : %s" % tags.inspect
+					rdebug "   doc tags : %s" % document_tags.inspect
+					document_tags.each do |tag|
+						words = tag.split(/\s+/)
+						if words.length > 1 then
+							words.each { |w| tags[w] += 1 }
+						else
+							tags[tag] += 1
+						end
+					end
+					rdebug "merged tags : %s" % tags.inspect
+
+					hit_count += 1 unless doc.cached?
+					rdebug "hit_count = %s" % hit_count
+					break if hit_count > limit
+				end
+			rescue Mendeley::Client::RateLimitExceeded => e
+				# try to resist ;-)
+			rescue Mendeley::Client::ClientError => e
+				raise e
+			end
+
+			# FIXME: cleanup mendeley-specific tags
+			# remove tags with non alpha characters
+			tags.keys.each do |tag|
+				tags.delete(tag) if tag.strip =~ /:/ ;
+			end
+			return tags
+		end
+		
+
+		#
+		# Search related tags on wikipedia
+		#
+		def self.related_tags_wikipedia config, intag, limit=RELATED_TAGS_DEFAULT_HITLIMIT
+			tags = Hash.new 0
+
+			page_json = config.wikipedia_client.request_page intag
+			page = Wikipedia::Page.new page_json
+			page.links.each { |tag| tags[tag] += 1 }
+
+			# FIXME: cleanup wikipedia-specific tags
+			return tags
+		end
+
+
+
 		# Return an hash of related tags with associated occurencies 
 		# for given input tag
-		def self.related_tags config, intag, algo=:default, limit=RELATED_TAGS_DEFAULT_HITLIMIT
+		def self.related_tags config, intag, limit=RELATED_TAGS_DEFAULT_HITLIMIT
 			# set count to zero
 			tags = Hash.new 0
 
-			case algo
-			when :default then
-				# try mendeley
-				if tags.empty? 
-					mendeley_related = related_tags config, intag, :mendeley
-					tags.merge!( mendeley_related ) do |key,oldval,newval|
-						oldval + newval
-					end
+			# try mendeley
+			if tags.empty? 
+				mendeley_related = related_tags_mendeley config, intag, limit
+				tags.merge!( mendeley_related ) do |key,oldval,newval|
+					oldval + newval
 				end
-				# try wikipedia
-				if tags.empty? 
-					wikipedia_related = related_tags config, intag, :wikipedia
-					tags.merge!( wikipedia_related ) do |key,oldval,newval|
-						oldval + newval
-					end
-				end
-				# or fail
-				
-			when :mendeley then
-				# get tags from documents
-				
-				begin
-					hit_count = 0
-					# using the API
-					Mendeley::Document.search_tagged config.mendeley_client, intag do |doc|
-						document_tags = doc.tags config.mendeley_client
-						rdebug "Merge document tags"
-						rdebug "common tags : %s" % tags.inspect
-						rdebug "   doc tags : %s" % document_tags.inspect
-						document_tags.each do |tag|
-							words = tag.split(/\s+/)
-							if words.length > 1 then
-								words.each { |w| tags[w] += 1 }
-							else
-								tags[tag] += 1
-							end
-						end
-						rdebug "merged tags : %s" % tags.inspect
-
-						hit_count += 1 unless doc.cached?
-						rdebug "hit_count = %s" % hit_count
-						break if hit_count > limit
-					end
-				rescue Mendeley::Client::RateLimitExceeded => e
-					# try to resist ;-)
-				end
-
-				# remove tags with non alpha characters
-				tags.keys.each do |tag|
-					tags.delete(tag) if tag.strip =~ /:/ ;
-				end
-
-				# using scrapping
-				#Mendeley::
-				#doc = Nokogiri::HTML(open('http://
-
-			when :wikipedia then
-				page_json = config.wikipedia_client.request_page intag
-				page = Wikipedia::Page.new page_json
-				page.links.each { |tag| tags[tag] += 1 }
-
-			else # :fail :-(
-				raise RelatedTagsFailure
-
 			end
+			# try wikipedia
+			if tags.empty? 
+				wikipedia_related = related_tags_wikipedia config, intag, limit
+				tags.merge!( wikipedia_related ) do |key,oldval,newval|
+					oldval + newval
+				end
+			end
+			# or fail
 
+			# FIXME: cleanup common tags
 			tags.delete(intag)
 			rdebug "result tags = %s" % tags.inspect
 			return tags
