84d7f408a7d868c8d728469de134d323dc8ab0d0
Sebastian Schawohl <sebastian.schawohl@gnuside.com>
2011-10-05 16:10:07 +0200

    Add tooltips for all types of relationships.

--- static/js/page_index.js
+++ static/js/page_index.js
@@ -5,7 +5,27 @@ $(document).ready(function() {
 		bodyHandler: function () {
 			return $("#tooltip_title").html();
 		},
-		extraClass: "tooltip_title",
+		track: true
+	});
+	$("li.tag_expected").tooltip({
+		bodyHandler: function () {
+			return $("#tooltip_expected").html();
+		},
+		extraClass: "tooltip_expected",
+		track: true
+	});
+	$("li.tag_controversial").tooltip({
+		bodyHandler: function () {
+			return $("#tooltip_controversial").html();
+		},
+		extraClass: "tooltip_controversial",
+		track: true
+	});
+	$("li.tag_aggregating").tooltip({
+		bodyHandler: function () {
+			return $("#tooltip_aggregating").html();
+		},
+		extraClass: "tooltip_aggregating",
 		track: true
 	});
 });
