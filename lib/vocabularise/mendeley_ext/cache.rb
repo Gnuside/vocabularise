c7fb03406edd9942085dcfbbe8c47cf10ce67b14
Glenn Y. Rolland <glenux@glenux.net>
2011-10-05 14:08:10 +0200

    vocabularise/mendeley_ext/cache: improve cache key format.

--- lib/vocabularise/mendeley_ext/cache.rb
+++ lib/vocabularise/mendeley_ext/cache.rb
@@ -27,7 +27,7 @@ module VocabulariSe ; module MendeleyExt
 			
 			url = create_url base, params
 			cache_used = false
-			cache_key = "mendeley:%s" % url
+			cache_key = "mendeley:%s,%s" % [ base, params ]
 			resp = nil
 
 			if @cache.include? url then
@@ -65,12 +65,10 @@ module VocabulariSe ; module MendeleyExt
 		#
 		#
 		def request_url base, params, &blk
-			
 			url = create_url base, params
-			cache_key = "mendeley:%s" % url
+			cache_key = _cache_key base, params
 			resp = nil
 
-			pp @cache
 			if @cache.include? cache_key then
 				rdebug "CACHE REQUEST %s%s" % [ base_api_url, url ]
 				resp = @cache[cache_key]
@@ -83,6 +81,16 @@ module VocabulariSe ; module MendeleyExt
 			return resp
 		end
 
+		private
+
+		def _cache_key base, params
+			id_str = params.to_a.
+				sort{ |a,b| a[0].to_s <=> b[0].to_s }.
+				map { |x| x.join '=' }.
+				join ','
+
+			cache_key = "mendeley:%s,%s" % [ base, id_str ]
+		end
 	end
 end ; end 
 
