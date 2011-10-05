11dec7355e3ddd6269bd13e8a88eda7f6b6b1cc0
Sebastian Schawohl <sebastian.schawohl@gnuside.com>
2011-10-05 15:46:42 +0200

    Add tooltip on page title.

--- static/js/page_index.js
+++ static/js/page_index.js
@@ -1,4 +1,11 @@
 
 $(document).ready(function() {
 	$('#searchwidget_query').focus();
+	$("#title").tooltip({
+		bodyHandler: function () {
+			return $("#tooltip_title").html();
+		},
+		extraClass: "tooltip_title",
+		track: true
+	});
 });
