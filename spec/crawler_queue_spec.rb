c5c0c1678f48fc7ddb08a1dc7ee5e9ea0721138b
Glenn Y. Rolland <glenux@gmail.com>
2011-10-08 15:56:21 +0200

    spec/crawler_queue_spec: improved crawler_queue_spec tests.

--- spec/crawler_queue_spec.rb
+++ spec/crawler_queue_spec.rb
@@ -19,6 +19,11 @@ require 'spec/spec_helper'
 
 describe 'CrawlerQueue' do
 
+	REQ_RELATED = "internal:related"
+	REQ_EXPECTED = "internal:expected"
+	REQ_AGGREGATING = "internal:aggregating"
+	REQ_OTHER = "other:noop"
+
 	before :all do
 		@queue = ::VocabulariSe::CrawlerQueue.new
 	end
@@ -28,133 +33,179 @@ describe 'CrawlerQueue' do
 	end
 
 	def helper_make_samepriority
-		@queue.push :related, 'love'
-		@queue.push :expected, 'death'
-		@queue.push :aggregating, 'love'
+		@queue.push "internal:related", 'love'
+		@queue.push "internal:expected", 'death'
+		@queue.push "internal:aggregating", 'love'
 	end
 
 	def helper_make_diffpriority
-		@queue.push :related, 'love', 2
-		@queue.push :expected, 'death', 0
-		@queue.push :aggregating, 'love', 1
+		@queue.push REQ_RELATED, 'love', 
+			VocabulariSe::CrawlerQueue::PRIORITY_HIGH
+		@queue.push REQ_EXPECTED, 'death', 
+			VocabulariSe::CrawlerQueue::PRIORITY_LOW
+		@queue.push REQ_AGGREGATING, 'love', 
+			VocabulariSe::CrawlerQueue::PRIORITY_NORMAL
 	end
 
-	#
-	it 'should be empty at start' do
-		@queue.should respond_to :empty?
+	describe '#size' do
+		it 'should be zero at start' do
+			@queue.size.should == 0
+		end
 
-		@queue.empty?.should == true
-		@queue.size.should == 0
+		it 'should be increased by each push' do
+			(1..100).each do |x|
+				@queue.push REQ_RELATED, "item#{x}"
+				@queue.size.should == x
+			end
+		end
+
+		it 'should be minored by each pop/shift' do
+		end
 	end
 
-	#
-	it 'should push (handle,query)' do
-		@queue.should respond_to :push
+	describe '#empty' do
+		it 'should respond true at start' do
+			@queue.should respond_to :empty?
 
-		@queue.push :related, 'love'
-		@queue.size.should == 1
-		puts @queue.size
-		@queue.push :expected, 'love'
-		puts @queue.size
-		@queue.size.should == 2
-		@queue.empty?.should == false
+			@queue.empty?.should == true
+		end
+
+		it 'should repond true at start (even with selector)' do
+			@queue.should respond_to :empty?
+
+			@queue.empty?.should == true
+		end
+
+		it 'should remove pushed content' do
+			@queue.should respond_to :empty!
+
+			helper_make_samepriority
+
+			@queue.size.should >= 0
+			@queue.empty!
+			@queue.size.should == 0
+			@queue.empty?.should == true
+		end
 	end
-
-
 	#
-	it 'should by empty-able' do
-		@queue.should respond_to :empty!
 
-		helper_make_samepriority
+	describe '#push' do
+		it 'should add t-uples of (handle,query)' do
+			@queue.should respond_to :push
 
-		@queue.size.should >= 0
-		@queue.empty!
-		@queue.size.should == 0
-		@queue.empty?.should == true
-	end
+			@queue.push :related, 'love'
+			@queue.size.should == 1
+			@queue.push :expected, 'love'
+			@queue.size.should == 2
+			@queue.empty?.should == false
+		end
 
+		it 'should add t-uples of (handle,query,priority)' do
+			@queue.should respond_to :push
 
-	#
-	it 'should allow test on included (handle,query)' do
-		@queue.should respond_to :include?
-
-		@queue.include?(:related, 'love').should == false
-		@queue.push :related, 'love'
-		@queue.include?(:related, 'love').should == true
-		@queue.include?(:expected, 'love').should == false
-	end
-
-
-	#
-	it 'should allow access to first' do
-		@queue.should respond_to :first
-
-		helper_make_samepriority
-
-		handler, query, priority = @queue.first
-		handler.to_sym.should == :related
-		query.should == 'love'
-	end
-
-
-	#
-	it 'should be shift-able' do
-		@queue.should respond_to :shift
-
-		helper_make_samepriority
-
-		@queue.shift.shift
-
-		handler, query, priority = @queue.first
-		handler.to_sym.should == :aggregating
-		query.should == 'love'
-	end
-
-
-	#
-	it 'should be pop-able' do
-		@queue.should respond_to :shift
-
-		helper_make_samepriority
-
-		@queue.shift
-		handler, query, priority = @queue.pop
-
-		handler.to_sym.should == :expected
-		query.should == 'death'
+			@queue.push REQ_RELATED, 'love', 
+				VocabulariSe::CrawlerQueue::PRIORITY_LOW
+			@queue.size.should == 1
+			@queue.push REQ_EXPECTED, 'love', 
+				VocabulariSe::CrawlerQueue::PRIORITY_HIGH
+			@queue.size.should == 2
+			@queue.empty?.should == false
+		end
 	end
 
 
 	#
-	it 'should not first an empty queue' do
 
-		lambda{ @queue.first }.should raise_error VocabulariSe::CrawlerQueue::EmptyQueueError
-	end
+	describe '#include?' do
+		#
+		it 'should allow test on included (handle,query)' do
+			@queue.should respond_to :include?
 
-	#
-	it 'should not shift an empty queue' do
-
-		lambda{ @queue.shift }.should raise_error VocabulariSe::CrawlerQueue::EmptyQueueError
-	end
-
-
-	#
-	it 'should iterate through entries' do
-		@queue.should respond_to :each
-
+			@queue.include?(:related, 'love').should == false
+			@queue.push :related, 'love'
+			@queue.include?(:related, 'love').should == true
+			@queue.include?(:expected, 'love').should == false
+		end
 	end
 
 
-	#
-	it 'should prioritize' do
+	describe '#first' do
+		it 'should allow access to first' do
+			@queue.should respond_to :first
+
+			helper_make_samepriority
+
+			handler, query, priority = @queue.first
+			handler.should == REQ_RELATED
+			query.should == 'love'
+		end
 	end
 
 
-	#
-	it 'should preserve order' do
-		@queue.push :A, 1
-		@queue.push :B, 2
-		@queue.push :C, 3
+	describe '#shift' do
+		it 'should be shift-able' do
+			@queue.should respond_to :shift
+
+			helper_make_samepriority
+
+			@queue.shift.shift
+
+			handler, query, priority = @queue.first
+			handler.should == REQ_AGGREGATING
+			query.should == 'love'
+		end
+	end
+
+
+	describe '#pop' do
+		it 'should be pop-able' do
+			@queue.should respond_to :shift
+
+			helper_make_samepriority
+
+			@queue.shift
+			handler, query, priority = @queue.pop
+
+			handler.should == REQ_EXPECTED
+			query.should == 'death'
+		end
+	end
+
+
+	describe '#first' do
+		#
+		it 'should fail on an empty queue' do
+
+			lambda{ @queue.first }.should raise_error VocabulariSe::CrawlerQueue::EmptyQueueError
+		end
+
+		#
+		it 'should not shift an empty queue' do
+
+			lambda{ @queue.shift }.should raise_error VocabulariSe::CrawlerQueue::EmptyQueueError
+		end
+
+		#
+		it 'should prioritize' do
+			pending("test not implemented")
+		end
+	end
+
+
+	describe '#each' do
+		it 'should iterate through entries' do
+			@queue.should respond_to :each
+
+			pending("test not implemented")
+		end
+
+		it 'should preserve order' do
+			@queue.push :A, 1
+			@queue.push :B, 2
+			@queue.push :C, 3
+
+			pending("test not implemented")
+		end
 	end
 
 end
