1ae3c6cbf0be73ccf0985b0b17439b5ab0ece923
Glenn Y. Rolland <glenux@gmail.com>
2011-10-09 18:08:49 +0200

    vocabularise/mendeley_ext/cache: remove obsolete code & Fix hit counting.

--- lib/vocabularise/mendeley_ext/cache.rb
+++ lib/vocabularise/mendeley_ext/cache.rb
@@ -25,50 +25,6 @@ module VocabulariSe ; module MendeleyExt
 
 		#
 		#
-		def old_request_url base, params, &blk
-			
-			url = create_url base, params
-			cache_used = false
-			cache_key = "mendeley:%s,%s" % [ base, params ]
-			resp = nil
-
-			if @cache.include? url then
-				rdebug "CACHE REQUEST %s%s" % [ base_api_url, url ]
-				resp = @cache[cache_key]
-				cache_used = true
-			else
-				# count a hit
-				rdebug "HIT TIMING %s%s" % [ base_api_url, url ]
-				@hit_counter.hit :mendeley
-
-				http = Net::HTTP.start(base_api_url.host, base_api_url.port) do |http|
-					rdebug "REAL  REQUEST %s%s" % [ base_api_url, url ]
-					resp = yield http
-
-					if ( resp["x-ratelimit-remaining"][0].to_i < RATELIMIT_EXCEEDED_LIMIT ) then
-						raise RateLimitExceeded, resp.header.inspect
-					end
-				end
-			end
-
-			json = JSON.parse resp.body
-			json[JSON_CACHE_KEY] = cache_used
-
-			case json[JSON_ERROR_KEY]
-			when /limit\s*exceeded/ then
-				raise RateLimitExceeded, resp.header.inspect
-			when /temporarily\s*unavailable/ then
-				raise ServiceUnavailable, resp.header.inspect
-			end
-			@cache[cache_key] = resp unless cache_used
-
-			rdebug "result = %s" % json.inspect
-			exit 1 #FIXME: debug
-			return json
-		end
-
-
-		#
 		#
 		def request_url base, params, &blk
 			url = create_url base, params
@@ -79,6 +35,10 @@ module VocabulariSe ; module MendeleyExt
 				rdebug "CACHE REQUEST %s%s" % [ base_api_url, url ]
 				resp = @cache[cache_key]
 			else
+				# count a hit
+				rdebug "HIT TIMING %s%s" % [ base_api_url, url ]
+				@hit_counter.hit :mendeley
+
 				rdebug "REAL  REQUEST %s%s" % [ base_api_url, url ]
 				resp = super base, params, &blk
 				@cache[cache_key] = resp
