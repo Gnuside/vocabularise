39458ce59d1320e4c6f3bdfeee1e549cbcb53fa2
Glenn Y. Rolland <glenux@gmail.com>
2011-10-10 01:10:28 +0200

    spec/mendeley_handler_spec: change tests.

--- spec/mendeley_handler_spec.rb
+++ spec/mendeley_handler_spec.rb
@@ -9,6 +9,8 @@ require 'rspec'
 require 'json'
 
 require 'vocabularise/model'
+require 'vocabularise/config'
+require 'vocabularise/crawler'
 require 'vocabularise/request_handler'
 require 'vocabularise/mendeley_handler'
 
@@ -23,32 +25,40 @@ describe 'RequestHandler' do
 			'cache_duration' => 7200,
 			'consumer_key' => 'd0d46ad71eb6691a44fb608424ad71c704e160d23',
 			'consumer_secret' => '4fb7cd67cd36e341be6966db0b4dd261',
-			'db_adapter' => 'sqlite3',
-			'db_database' => 'tmp/test/crawler.sqlite3'
+			"db_adapter" => "mysql",
+			"db_database" => "vocabularise_test",
+			"db_host" => "localhost",
+			"db_username" => "vocabularise",
+			"db_password" => "vocapass"
 		}
 
+		@config = VocabulariSe::Config.new json
+		@crawler = VocabulariSe::Crawler.new @config
+
 		# set crawler
+		@crawler.run
 	end
 
 	before :each do
+		VocabulariSe::QueueEntry.all.destroy
 	end
 
-	#
-	it 'should list handlers' do
-		#th = TestHandler.new nil, nil
-		handlers = VocabulariSe::RequestHandler.subclasses
-		handlers.include?(TestHandler).should == true
-	end
+	it 'should respond to HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE' do
+		intag = "love"
 
-	it 'should correctly match the handle' do
-		TestHandler.should respond_to :handles?
-		TestHandler.handles?(:good_example).should == true
-		TestHandler.handles?(:good_other_example).should == true
-		TestHandler.handles?(:bad_example).should == false
-	end
-
-	it 'should process the handle, query, priority, etc.' do
-		pending("implement it")
+		page0 = nil
+		#related_tags = VocabulariSe::Utils.related_tags config, intag
+		loop do 
+			begin
+				page0 = @crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE,
+					{ "tag" => intag, "page" => 0 }, VocabulariSe::Crawler::MODE_INTERACTIVE
+			rescue VocabulariSe::Crawler::DeferredRequest => e
+				puts "deferred" + e.message
+				puts e.backtrace
+				sleep 0.1
+			end
+		end
+		pp page0
 	end
 
 	#
