4e84c637653a3d6d196835d6fd555a36984ad871
Glenn Y. Rolland <glenux@gmail.com>
2011-10-09 17:41:21 +0200

    mendeley/document: update code to use long json only.

--- lib/mendeley/document.rb
+++ lib/mendeley/document.rb
@@ -27,13 +27,13 @@ module Mendeley
 		# Instance
 		#
 		def title 
-			@short_json["title"]
+			@long_json["title"]
 		end
 
 		# Instance
 		#
 		def uuid
-			@short_json["uuid"]
+			@long_json["uuid"]
 		end
 
 		# tags
@@ -55,60 +55,5 @@ module Mendeley
 			@long_json["stats"]["readers"].to_i
 		end
 
-		# FIXME: remove hard limit
-		#DOCUMENTS_TAGGED_PAGE_LIMIT = 10
-
-		#
-		# Static
-		#
-		# search_tagged
-		#   * if cached return result
-		#   * look for all pages _from cache_
-		#   * +request two more pages (search_tagged_page tag, page)
-		#   * for cached pages, compute tags on documents
-		#   * add computation in cache
-		# 
-		# search_tagged_callback:
-		#
-		#
-		#
-=begin
-		def self.search_tagged client, tag, &blk
-			page = 0
-			total_pages = 0
-			documents = []
-			while true do
-					# first json snippets count as a hit
-					# but all following count as cached
-					cached = false
-					begin
-						resp = client.documents_tagged({
-							:tag => tag, 
-							:page => page 
-							#		:limit => DOCUMENTS_TAGGED_LIMIT 
-						})
-					rescue Mendeley::Client::ClientError => e
-						# got no document list
-						break
-					end
-					total_pages = resp["total_pages"]
-					if resp["documents"].nil? then
-						pp resp 
-						next
-					end
-					resp["documents"].each do |resp_doc|
-						resp_doc[JSON_CACHE_KEY] = cached
-						doc = Document.new resp_doc
-						documents << doc
-						yield doc if block_given?
-						cached = true
-					end
-					page += 1
-					break if page >= total_pages
-					break if page >= DOCUMENTS_TAGGED_PAGE_LIMIT
-			end
-			return documents
-		end
-=end
 	end
 end
