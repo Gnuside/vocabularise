8bd584337ad52234a817b43aa6168cece631a997
Glenn Y. Rolland <glenux@gmail.com>
2011-11-02 01:37:29 +0100

    spec/mendeley_handler_spec: update spec for mendeley requests

--- spec/mendeley_handler_spec.rb
+++ spec/mendeley_handler_spec.rb
@@ -11,6 +11,7 @@ require 'json'
 require 'vocabularise/model'
 require 'vocabularise/config'
 require 'vocabularise/crawler'
+require 'vocabularise/queue'
 require 'vocabularise/request_handler'
 require 'vocabularise/mendeley_handler'
 
@@ -19,11 +20,27 @@ require 'spec/spec_helper'
 
 describe 'RequestHandler' do
 
+	SLEEP_TIME = 5
+
+	def helper_request &blk
+		result = nil
+		while result.nil? do 
+			begin
+				result = yield 
+			rescue VocabulariSe::Crawler::DeferredRequest => e
+				# retry ;-)
+				sleep SLEEP_TIME
+			end
+		end
+		return result
+	end
+
 	before(:all) do
 		FileUtils.mkdir_p "tmp/test"
 		json = {
 			'cache_dir' => 'tmp/test/cache',
-			'cache_duration' => 7200,
+			'cache_duration_min' => 3600,
+			'cache_duration_max' => 7200,
 			'consumer_key' => 'd0d46ad71eb6691a44fb608424ad71c704e160d23',
 			'consumer_secret' => '4fb7cd67cd36e341be6966db0b4dd261',
 
@@ -44,6 +61,7 @@ describe 'RequestHandler' do
 		@config = VocabulariSe::Config.new json
 		@crawler = VocabulariSe::Crawler.new @config
 
+		#DataMapper::Logger.new(STDERR, :debug)
 		# set crawler
 		@crawler.run
 	end
@@ -52,22 +70,37 @@ describe 'RequestHandler' do
 		VocabulariSe::QueueEntry.all.destroy
 	end
 
+	it 'should respond to HANDLE_MENDELEY_DOCUMENT_DETAILS' do
+		uuid = '47df8a70-83f6-11df-aedb-0024e8453de8'
+
+		doc = helper_request do
+			@crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_DETAILS,
+				{ "uuid" => uuid }
+		end
+		#pp doc
+	end
+
 	it 'should respond to HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE' do
 		intag = "love"
 
-		page0 = nil
-		#related_tags = VocabulariSe::Utils.related_tags config, intag
-		loop do 
-			begin
-				page0 = @crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE,
-					{ "tag" => intag, "page" => 0 }, VocabulariSe::Crawler::MODE_INTERACTIVE
-				break
-			rescue VocabulariSe::Crawler::DeferredRequest => e
-				#puts "deferred" + e.message
-				#puts e.backtrace
-			end
+		page = helper_request do
+			@crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE,
+				{ "tag" => intag, "page" => 0 }
 		end
-		pp page0
+		#pp page
+	end
+
+	it 'should respond to HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED' do
+		intag = "love"
+
+		docs = helper_request do
+			@crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED,
+				{ "tag" => intag }
+		end
+
+		if docs.size > 0 then
+			docs.first.kind_of?(Mendeley::Document).should == true
+		end
 	end
 
 	#
