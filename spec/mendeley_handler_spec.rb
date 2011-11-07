0a5b16c16ba8de4ed4283fc47dc15bea1280418b
Glenn Y. Rolland <glenux@gmail.com>
2011-11-07 23:25:50 +0100

    spec/mendeley_handler_spec: Make interactive

--- spec/mendeley_handler_spec.rb
+++ spec/mendeley_handler_spec.rb
@@ -71,26 +71,32 @@ describe 'RequestHandler' do
 	end
 
 	it 'should respond to HANDLE_MENDELEY_DOCUMENT_DETAILS' do
+		STDOUT.puts "press [enter] to start"
+		STDIN.gets
 		uuid = '47df8a70-83f6-11df-aedb-0024e8453de8'
 
 		doc = helper_request do
 			@crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_DETAILS,
 				{ "uuid" => uuid }
 		end
-		#pp doc
+		pp doc
 	end
 
 	it 'should respond to HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE' do
+		STDOUT.puts "press [enter] to start"
+		STDIN.gets
 		intag = "love"
 
 		page = helper_request do
 			@crawler.request VocabulariSe::HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED_PAGE,
 				{ "tag" => intag, "page" => 0 }
 		end
-		#pp page
+		pp page
 	end
 
 	it 'should respond to HANDLE_MENDELEY_DOCUMENT_SEARCH_TAGGED' do
+		STDOUT.puts "press [enter] to start"
+		STDIN.gets
 		intag = "love"
 
 		docs = helper_request do
