0fc2a0a82c3ba048abe287affd38fca725545ba7
Glenn Y. Rolland <glenux@gmail.com>
2011-11-28 21:54:33 +0100

    vocabularise/controversial_handler: Fix latests issues.

--- lib/vocabularise/controversial_handler.rb
+++ lib/vocabularise/controversial_handler.rb
@@ -5,21 +5,22 @@ module VocabulariSe
 
 	HANDLE_INTERNAL_CONTROVERSIAL = "internal:controversial"
 
-	class InternalAggregating < RequestHandler
+	class InternalControversial < RequestHandler
 
 		handles HANDLE_INTERNAL_CONTROVERSIAL
 		cache_result DURATION_SHORT
 
 		# limit the number of considered articles for computation               
-		ARTICLE_LIMIT = 3                                                       
+		ARTICLE_LIMIT = 3
 
 		def tag_hotness tags_arr                                        
 			search_expr = '"%s"' % tags_arr.sort.join('" AND "')                
 			puts search_expr                                                    
 
-			raise NotImplementedError
+			resp_json = @crawler.request \
+				HANDLE_WIKIPEDIA_SEARCH,
+				{ "query" => search_expr }
 
-			resp_json =  config.wikipedia_client.search( search_expr )          
 			resp = JSON.parse resp_json                                         
 
 			score = 0                                                           
@@ -28,14 +29,13 @@ module VocabulariSe
 				talk_title = "Talk:%s" % article_desc["title"]                  
 				puts "  - " + talk_title                                        
 				#pp article_desc                                                
-				page_json = config.wikipedia_client.request_page talk_title     
-				begin                                                           
-					page = Wikipedia::Page.new page_json                        
-					raw = page.content                                          
-				rescue Timeout::Error => e                                      
-					puts "  WARNING: timeout error"                             
-					next                                                        
-				end                                                             
+				page_json = @crawler.request \
+					HANDLE_WIKIPEDIA_REQUEST_PAGE,
+					{ "page" => talk_title }
+
+				page = Wikipedia::Page.new page_json                        
+				raw = page.content                                          
+
 				#puts raw                                                       
 				titles = (raw.split(/\n/) rescue []).select{ |line| line.strip =~ /^\s*==.*==\s*/ } 
 				score += titles.size                                            
@@ -44,7 +44,7 @@ module VocabulariSe
 				limit -= 1                                                      
 				break if limit <= 0                                             
 			end                                                                 
-			puts "  * score = %s" % score                                       
+			#puts "  * score = %s" % score                                       
 			return score                                                        
 		end 
 
