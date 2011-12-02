6f9f03d36e8a31a677d9c4f29e0d873ca219619b
Glenn Y. Rolland <glenux@gmail.com>
2011-12-02 13:07:29 +0100

    vocabularise/controversial_handler: Cleanup & added links.

--- lib/vocabularise/controversial_handler.rb
+++ lib/vocabularise/controversial_handler.rb
@@ -10,43 +10,52 @@ module VocabulariSe
 		handles HANDLE_INTERNAL_CONTROVERSIAL
 		cache_result DURATION_SHORT
 
-		# limit the number of considered articles for computation               
+		# limit the number of considered articles for computation
 		ARTICLE_LIMIT = 3
 
-		def tag_hotness tags_arr                                        
-			search_expr = '"%s"' % tags_arr.sort.join('" AND "')                
-			puts search_expr                                                    
+		def tag_hotness tags_arr
+			links = []
+			search_expr = '"%s"' % tags_arr.sort.join('" AND "')
+			puts search_expr
 
 			resp_json = @crawler.request \
 				HANDLE_WIKIPEDIA_SEARCH,
 				{ "query" => search_expr }
 
-			resp = JSON.parse resp_json                                         
+			resp = JSON.parse resp_json
 
-			score = 0                                                           
-			limit = ARTICLE_LIMIT                                               
-			resp["query"]["search"].each do |article_desc|                      
-				talk_title = "Talk:%s" % article_desc["title"]                  
-				puts "  - " + talk_title                                        
-				#pp article_desc                                                
+			score = 0
+			limit = ARTICLE_LIMIT
+			resp["query"]["search"].each do |article_desc|
+				links << {
+					:text => article_desc["title"],
+					:url => "http://en.wikipedia.org/wiki/%s" % article_desc["title"]
+				}
+				talk_title = "Talk:%s" % article_desc["title"]
+				puts "  - " + talk_title
+				#pp article_desc
 				page_json = @crawler.request \
 					HANDLE_WIKIPEDIA_REQUEST_PAGE,
 					{ "page" => talk_title }
 
-				page = Wikipedia::Page.new page_json                        
-				raw = page.content                                          
+				begin
+					page = Wikipedia::Page.new page_json
+					raw = page.content
+				rescue Timeout::Error => e                                                                              
+					# FIXME
+				end                                                                                                     
 
-				#puts raw                                                       
-				titles = (raw.split(/\n/) rescue []).select{ |line| line.strip =~ /^\s*==.*==\s*/ } 
-				score += titles.size                                            
-				#page.links.each { |tag| tags[tag] += 1 }                       
+				#puts raw
+				titles = (raw.split(/\n/) rescue []).select{ |line| line.strip =~ /^\s*==.*==\s*/ }
+				score += titles.size
+				#page.links.each { |tag| tags[tag] += 1 }
 
-				limit -= 1                                                      
-				break if limit <= 0                                             
-			end                                                                 
-			#puts "  * score = %s" % score                                       
-			return score                                                        
-		end 
+				limit -= 1
+				break if limit <= 0
+			end
+			#puts "  * score = %s" % score
+			return score, links
+		end
 
 		process do |handle, query, priority|
 			@debug = true
@@ -56,25 +65,26 @@ module VocabulariSe
 			intag = query['tag']
 			raise ArgumentError, "'tag' must not be nil" if intag.nil?
 
-			# Association audacieuse                                            
-			workspace = {}                                                      
-			documents = Set.new                                                 
+			# Association audacieuse
+			workspace = {}
+			documents = Set.new
 			related_tags = @crawler.request \
-				HANDLE_INTERNAL_RELATED_TAGS, 
+				HANDLE_INTERNAL_RELATED_TAGS,
 				{ "tag" => intag }
 
-			related_tags.each do |reltag,reltag_count|                          
-				hotness = tag_hotness( [reltag, intag] )                
+			related_tags.each do |reltag,reltag_count|
+				hotness, links = tag_hotness( [reltag, intag] )
 
-				workspace[reltag] = {                                           
-					:hotness => hotness                                         
-				}                                                               
-			end                                                                 
+				workspace[reltag] = {
+					:hotness => hotness,
+					:links => links
+				}
+			end
 
-			# sort workspace keys (tags) by slope                               
+			# sort workspace keys (tags) by slope
 			result = workspace.sort{ |a,b| a[1][:hotness] <=> b[1][:hotness] }.reverse
 
-			# FIXME : use archive pages if needed                               
+			# FIXME : use archive pages if needed
 			return result
 		end
 	end
