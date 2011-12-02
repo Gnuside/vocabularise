f75eda3cbb339f9270ab8c57ca4cdbb238e89d46
Glenn Y. Rolland <glenux@gmail.com>
2011-12-02 15:19:59 +0100

    deploy: Add testing rules.

---
+++ config/deploy/testing.rb
@@ -0,0 +1,13 @@
+
+role :web, "vocabulari.se"                          # Your HTTP server, Apache/etc
+role :app, "vocabulari.se"                          # This may be the same as your `Web` server
+role :db,  "vocabulari.se", :primary => true # This is where Rails migrations will run
+
+set :user, "vocabularise"
+set :use_sudo, false
+
+set :deploy_to, "/home/vocabularise/beta"
+set :deploy_env, 'production'
+
+# limit number of releases on server
+set :keep_releases, 5
