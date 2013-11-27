80dea7450be12378a9c17d2b8824a917200dcf97
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-11-27 01:29:35 +0100

    Split configuration parsing & validation.

--- lib/vocabularise/config.rb
+++ lib/vocabularise/config.rb
@@ -30,7 +30,7 @@ module VocabulariSe
 		attr_reader :dictionary
 
 		def initialize json
-			# default values
+			## default values
 			@cache = nil
 			@mendeley_client = nil
 			@wikipedia_client = nil
@@ -38,21 +38,11 @@ module VocabulariSe
 			@database = nil
 			@dictionary = nil
 
+			validate_json json
 			load_json json
-			validate	
-		end
-
-		def validate
-			raise ConfigurationError, "Cache not defined" if @cache.nil?
-			#raise ConfigurationError, "No mendeley client" if @mendeley_client.nil?
 		end
 
 		def load_json json
-			#raise ConfigurationError, "no cache_dir specified" unless json.include? "cache_dir"
-
-			raise ConfigurationError, "no db_adapter specified" unless json.include? "db_adapter"
-			raise ConfigurationError, "no db_database specified" unless json.include? "db_database"
-
 			case json["db_adapter"]
 			when 'sqlite','sqlite3' then
 				@database = {
@@ -64,9 +54,6 @@ module VocabulariSe
 					"timeout"   => 15000
 				}
 			when 'mysql' then
-				raise ConfigurationError, "no db_password specified" unless json.include? "db_password"
-				raise ConfigurationError, "no db_username specified" unless json.include? "db_username"
-				raise ConfigurationError, "no db_host specified" unless json.include? "db_host"
 				@database = {	
 					"adapter"   => 'mysql',
 					"database"  => json["db_database"],
@@ -74,9 +61,6 @@ module VocabulariSe
 					"password"  => json["db_password"],
 					"host"      => json["db_host"]
 				}	
-			else
-				STDERR.puts json.inspect
-				raise RuntimeError, "unknown database adapter"
 			end
 
 			DataMapper::Logger.new(STDERR, :info)
@@ -86,19 +70,13 @@ module VocabulariSe
 			DataMapper::Model.raise_on_save_failure = true                      
 			DataMapper.auto_upgrade!                                            
 
-			## FIX: initialize delayed job
+			## initialize delayed job
 			Delayed::Worker.max_run_time = 900
 			Delayed::Worker.backend = :data_mapper
 
-			raise ConfigurationError, "no dictionary specified" unless json.include? "dictionary"
 			@dictionary = json['dictionary']
 
 			# setup cache & queue
-			# 2 hours
-			raise ConfigurationError, "no consumer_key specified" unless json.include? "consumer_key"
-			raise ConfigurationError, "no cache_duration_min specified" unless json.include? "cache_duration_min"
-			raise ConfigurationError, "no cache_duration_max specified" unless json.include? "cache_duration_max"
-
 			@cache = VocabulariSe::Cache.new( json["cache_duration_min"], json["cache_duration_max"] )
 
 			@counter = HitCounter.new
@@ -115,7 +93,38 @@ module VocabulariSe
 			@wikipedia_client.extend(::VocabulariSe::WikipediaExt::Cache)
 			@wikipedia_client.cache = cache
 			@wikipedia_client.hit_counter = @counter
+		end
 
+		def validate_json json
+
+			## database configuration
+			raise ConfigurationError, "no db_adapter specified" unless json.include? "db_adapter"
+			raise ConfigurationError, "no db_database specified" unless json.include? "db_database"
+
+			case json["db_adapter"]
+			when 'sqlite','sqlite3' then
+				raise ConfigurationError, "db_database must be a file" unless File.exist? json['db_database']
+				# nothing
+			when 'mysql' then
+				raise ConfigurationError, "no db_password specified" unless json.include? "db_password"
+				raise ConfigurationError, "no db_username specified" unless json.include? "db_username"
+				raise ConfigurationError, "no db_host specified" unless json.include? "db_host"
+			else
+				STDERR.puts json.inspect
+				raise RuntimeError, "unknown database adapter"
+			end
+
+			## seed
+			raise ConfigurationError, "no dictionary specified" unless json.include? "dictionary"
+
+			## mendeley API
+			raise ConfigurationError, "no consumer_key specified" unless json.include? "consumer_key"
+			raise ConfigurationError, "no consumer_secret specified" unless json.include? "consumer_secret"
+
+			## cache configuration
+			# raise ConfigurationError, "no cache_dir specified" unless json.include? "cache_dir"
+			raise ConfigurationError, "no cache_duration_min specified" unless json.include? "cache_duration_min"
+			raise ConfigurationError, "no cache_duration_max specified" unless json.include? "cache_duration_max"
 		end
 	end
 end
